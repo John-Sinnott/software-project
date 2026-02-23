04d6f1ca43e55ceb09e4b32c69ba180c5f788d71
John Sinnott~ <N00240449@iadt.ie~>
2026-02-23 12:34:04 +0000

    23/02/2026 initial commit

---
+++ resources/views/software/index.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        </h2>
+    </x-slot>
+
+   
+                    {{-- shows all Existing Stocks for the specific drink--}}
+                
+                    <h4 class="font-semibold text-md mt-8">Rating</h4>
+            
+         
+</x-app-layout>
